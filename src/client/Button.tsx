3e1585e67017964750be851ff4994a492af7a12b
wigillette <wgillette02@gmail.com>
2021-12-29 17:40:41 -0500

    Button BackgroundColor Tween

--- src/client/Button.tsx
+++ src/client/Button.tsx
@@ -1,5 +1,5 @@
 import Roact from "@rbxts/roact";
-import { Players } from "@rbxts/services";
+import { Players, TweenService } from "@rbxts/services";
 const playerGui = Players.LocalPlayer.WaitForChild("PlayerGui") as PlayerGui;
 
 interface UIProps {
@@ -12,7 +12,7 @@ interface UIState {
 class Button extends Roact.Component<UIProps, UIState> {
 	textButtonRef: Roact.Ref<TextButton>;
 	state = {
-		backgroundColor: new Color3(),
+		backgroundColor: Color3.fromRGB(200, 0, 0),
 	};
 
 	constructor(props: UIProps) {
@@ -29,16 +29,33 @@ class Button extends Roact.Component<UIProps, UIState> {
 					Position={new UDim2(0.4, 0, 0.1, 0)}
 					Size={new UDim2(0.2, 0, 0.1, 0)}
 					BackgroundColor3={this.state.backgroundColor}
-					BackgroundTransparency={0.5}
+					BackgroundTransparency={0}
 					Ref={this.textButtonRef}
+					AutoButtonColor={false}
 					Font={"TitilliumWeb"}
 					Event={{
 						MouseEnter: () => {
+							const textButton = this.textButtonRef.getValue() as TextButton;
+							const tween = TweenService.Create(
+								textButton,
+								new TweenInfo(0.2, Enum.EasingStyle.Quad, Enum.EasingDirection.Out, 0, false, 0),
+								{ BackgroundColor3: Color3.fromRGB(255, 0, 0) },
+							);
+							tween.Play();
+							wait(0.2);
 							this.setState({
 								backgroundColor: Color3.fromRGB(255, 0, 0),
 							});
 						},
 						MouseLeave: () => {
+							const textButton = this.textButtonRef.getValue() as TextButton;
+							const tween = TweenService.Create(
+								textButton,
+								new TweenInfo(0.2, Enum.EasingStyle.Quad, Enum.EasingDirection.Out, 0, false, 0),
+								{ BackgroundColor3: Color3.fromRGB(220, 0, 0) },
+							);
+							tween.Play();
+							wait(0.2);
 							this.setState({
 								backgroundColor: Color3.fromRGB(220, 0, 0),
 							});
